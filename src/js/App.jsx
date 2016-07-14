7dbbf805706029adefb388f20558ee0fc2aa863c
Haroon Ghawsi <ghawsiharoon@gmail.com>
2016-07-14 16:38:23 +0200

    added localStorage for saving tasks

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,49 +1,76 @@
 import React from "react";
+import localStorage from "localStorage";
 
 var TodoList = React.createClass({
     render: function(){
         var createItem = function(item){
-            return <li key={item.id}>{item.text}</li>;
+            return <li key={item.id}> {item.text}</li>;
         };
         return <ul>{this.props.items.map(createItem)}</ul>;
     }
 });
+
 var TodoApp = React.createClass({
     getInitialState: function(){
-        return {items: [], text: ''};
+         var data = localStorage.getItem('nextItems');
+         var items = [];
+        
+        items = JSON.parse(data);
+        
+         var dailyList = [];
+         var weeklyList = [];
+         var monthyList = [];
+         var yearlyList = [];
+
+         for (var i=0; i<items.length;i++) {
+             if (items[i].interval === 'd') {
+                 dailyList.push(items[i]);
+             }else if(items[i].interval === 'w'){
+                 weeklyList.push(items[i]);
+             }else if(items[i].interval === 'm'){
+                 monthyList.push(items[i]);
+             }else if(items[i].interval === 'y')
+                 yearlyList.push(items[i]);
+         }
+
+        return {items: items, text: '', interval: ''};
     },
     onChange: function(e){
         this.setState({text: e.target.value});
     },
     handleSubmit: function(e){
         e.preventDefault();
-        var nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+        var newItem = {text: this.state.text, id: Date.now(), interval: this.state.interval};
+        var nextItems = this.state.items.concat([newItem]);
         var nextText = '';
+
+        localStorage.setItem('nextItems' , JSON.stringify(nextItems));
         this.setState({items: nextItems, text: nextText});
     },
-    onClickItems: function(e){
+    onClickItems: function(){
         this.state.items.pop();
+        localStorage.setItem('nextItems' , JSON.stringify(this.state.items));
         this.setState(this.state);
     },
+    onChangeSelection: function(e){
+        var value = e.target.value;
+        this.setState({interval: value});
+    },
 
-    render: function(){
+   render: function(){
         return(
             <div>
                 <h1>Welcome to To Do Web Application!</h1>
                 <h3>Please add Tasks below</h3>
+                 <select onChange={this.onChangeSelection} value={this.value}>
+                    <option value="T">Select Task Type</option>
+                    <option value="d">Daily</option>
+                    <option value="w">Weekly</option>
+                    <option value="m">Monthly</option>
+                    <option value="y">Yearly</option>
+                </select>
                     <TodoList items={this.state.items} />
-                    <input class="form-control" id="inputDefault" type="text" onChange={this.onChange} value={this.state.text}/>
-                            <div class="btn-group">
-                                <a  class="btn btn-warning">Scheduler</a>
-                                <a aria-expanded="true" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li>Daily</li>
-                                    <li>Weekly</li>
-                                    <li>Monthly</li>
-                                    <li>Yearly</li>
-                                </ul>
-                            </div>
-                            
+                    <input class="form-control" id="inputDefault" type="text" onChange={this.onChange} value={this.state.text}/>      
                     <button class="btn btn-success" onClick={this.handleSubmit}>{'Add'}</button>
                     <button class="btn btn-danger" onClick={this.onClickItems}>{'Remove'}</button>
             </div>
